8af54d11d44cca64a1f3189062cee13005dd88cb
Jonathan Martins <jon.martinsu@gamil.com>
2018-09-02 13:18:21 -0300

    Initial Commit

---
+++ JonAlert/JonAlert.h
@@ -0,0 +1,19 @@
+//
+//  JonAlert.h
+//  JonAlert
+//
+//  Created by Jonathan Martins on 02/09/2018.
+//  Copyright © 2018 Surrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JonAlert.
+FOUNDATION_EXPORT double JonAlertVersionNumber;
+
+//! Project version string for JonAlert.
+FOUNDATION_EXPORT const unsigned char JonAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JonAlert/PublicHeader.h>
+
+
